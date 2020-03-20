22efc8fa7f3cd975646203b940572d8f3c0ecd48
972657750 <61618632+972657750@users.noreply.github.com>
2020-03-20 15:30:48 +0800

    Update 随机数计数.py

--- 随机数计数.py
+++ 随机数计数.py
@@ -1,9 +1,9 @@
 import random
 times=0
 dict={}
-while times <1000:
+while times < 1000:#这里输入你要生成的随机数个数
     times=times+1
-    number=random.randint(0,100)
+    number=random.randint(0,100)#这里输入你要生成随机数的范围
     if number in dict.keys():
         dict[number]=dict[number]+1
     else:
